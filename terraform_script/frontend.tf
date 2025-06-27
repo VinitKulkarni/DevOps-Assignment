e757b244caa2d293267f81cc5d3513ca84653e86
VinitKulkarni <42830279+VinitKulkarni@users.noreply.github.com>
2025-06-27 22:11:02 +0530

    Delete terraform_script/frontend.tf

--- terraform_script/frontend.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_ecs_task_definition" "frontend" {
-  container_definitions = jsonencode([
-    {
-      name  = "frontend"
-      image = "123456789012.dkr.ecr.ap-south-1.amazonaws.com/frontend:9f1754fa78c3bc814331bee18ab668dd879e3945"
-      ...
-    }
-  ])
-}
