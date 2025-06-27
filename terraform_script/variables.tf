77f55c84623027a20724b2a8d27526d5427c7250
VinitKulkarni <42830279+VinitKulkarni@users.noreply.github.com>
2025-06-27 22:14:35 +0530

    Create variables.tf

---
+++ terraform_script/variables.tf
@@ -0,0 +1,15 @@
+variable "availability_zone_1" {
+  type = string
+}
+
+variable "availability_zone_2" {
+  type = string
+}
+
+variable "frontend_image"{
+  type = string
+}
+
+variable "backend_image"{
+  type = string
+}
