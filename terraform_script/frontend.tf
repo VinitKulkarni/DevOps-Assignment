e2f89536576fc5dc69324bbaeb67ddcab85dcf12
VinitKulkarni <42830279+VinitKulkarni@users.noreply.github.com>
2025-06-27 17:42:27 +0530

    Update frontend.tf

--- terraform_script/frontend.tf
+++ terraform_script/frontend.tf
@@ -2,7 +2,7 @@ resource "aws_ecs_task_definition" "frontend" {
   container_definitions = jsonencode([
     {
       name  = "frontend"
-      image = "123456789012.dkr.ecr.ap-south-1.amazonaws.com/frontend:<REPLACE_TAG>"
+      image = "123456789012.dkr.ecr.ap-south-1.amazonaws.com/frontend:fe4402d347421881545fbb9ba129cbcd5e0023d0"
       ...
     }
   ])
