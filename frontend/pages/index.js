048e09589e065b2a0f110bf4521b533a4ac13d08
VinitKulkarni <42830279+VinitKulkarni@users.noreply.github.com>
2025-06-28 13:07:39 +0530

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -52,7 +52,7 @@ export default function Home() {
         </div>
         <div className="info">
           <p>Backend URL: {apiUrl}</p>
-          <p>build by vinit Date:28june-12:25pm</p>
+          <p>build by vinit Date:28june-01:07pm</p>
         </div>
       </main>
 
