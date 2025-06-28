32c60345042d0f3baf8ad9f9145cca7213a6f529
VinitKulkarni <42830279+VinitKulkarni@users.noreply.github.com>
2025-06-28 12:17:07 +0530

    Update variables.tf

--- terraform_script/variables.tf
+++ terraform_script/variables.tf
@@ -6,10 +6,22 @@ variable "availability_zone_2" {
   type = string
 }
 
-variable "frontend_image"{
+variable "frontend_image" {
   type = string
 }
 
-variable "backend_image"{
+variable "backend_image" {
+  type = string
+}
+
+variable "vpc_cidr_block" {
+  type = string
+}
+
+variable "publicSubnet1a_cidr_block" {
+  type = string
+}
+
+variable "publicSubnet1b_cidr_block" {
   type = string
 }
